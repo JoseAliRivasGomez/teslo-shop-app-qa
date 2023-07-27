e78a99e473a3a24e3fee2b5526e56d89959481cb
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2023-07-26 18:39:28 -0600

    done

--- android/app/src/main/kotlin/com/joserivasgomez/tesloshop/MainActivity.kt
+++ android/app/src/main/kotlin/com/joserivasgomez/tesloshop/MainActivity.kt
@@ -4,14 +4,15 @@ import io.flutter.embedding.android.FlutterActivity
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
+import android.os.Bundle
 
 class MainActivity: FlutterActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        AppCenter.start(getApplication(), "0cb17681-bcd7-4b49-a0b8-103ef93c951c",
-                  Analytics.class, Crashes.class);
+        AppCenter.start(getApplication(), "0cb17681-bcd7-4b49-a0b8-103ef93c951c", Analytics::class.java, Crashes::class.java)
+
 
     }
 
